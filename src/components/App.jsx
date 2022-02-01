2380d9325c35a80c4cb2940b64a66b661f2a33b3
Andrew Talle <talle.andrew@gmail.com>
2022-02-01 13:37:09 -0700

    setup input and display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Display from './Display';
+import QueryInput from './QueryInput';
 
 import '../assets/css/App.css';
 
@@ -6,7 +8,8 @@ function App() {
   return (
     <div>
       <h1>QLEO</h1>
-
+      <QueryInput />
+      <Display />
     </div>
   );
 }
