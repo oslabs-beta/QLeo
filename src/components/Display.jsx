5b21431b82bcd7bedba41fe1377fdcdcddb42d14
Andrew Talle <talle.andrew@gmail.com>
2022-02-02 15:37:05 -0700

    setup queryTimer

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 function Display({ query }) {
+  const [time, setTime] = useState();
+
   return query !== '' ? (
     <div className='display-container'>
       <h1>Display Component</h1>
-      <div className="query-time">Query Response Time</div>
+      <div className="query-time">Query Response Time: {time}</div>
       <div className="resolver-breakdown">Resolver Breakdown:</div>
-      <Detail query={query} />
+      <Detail query={query} setTime={setTime} />
     </div>
   ) : (
     <p>No Results to display...</p>
   );
 }
 
-function Detail({ query }) {
+function Detail({ query, setTime }) {
   const queryObj = gql`${query}`;
-  const { loading, error, data } = useQuery(queryObj);
+
+  const t0 = performance.now();
+
+  const { loading, error, data } = useQuery(queryObj, {
+    onCompleted: data => {
+      const t1 = performance.now();
+      setTime(t1 - t0);
+      console.log(`Call to query took ${t1 - t0} milliseconds.`); 
+      console.log(data);
+    },
+  });  
 
   if (loading) {
     return <p>Loading...</p>;
