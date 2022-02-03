7afb775f93dd8e6aa1e038ac9b1ca63e82ac5e58
Chon Hou Ho <chonhouh@gmail.com>
2022-02-03 14:42:50 -0800

    added tracker

--- server/server.js
+++ server/server.js
@@ -1,45 +1,24 @@
 const typeDefs = require('./src/schema');
 const resolvers = require('./src/resolvers');
 const { ApolloServer } = require('apollo-server');
+const { ApolloServerPluginInlineTrace } = require('apollo-server-core');
+const plugins = require('./plugins/tracker');
 
 const  PORT = 3000;
-
-const LogPlugin = {
-  requestDidStart(requestContext) {
-    const start = Date.now();
-    let op;
-
-    return {
-      didResolveOperation (context) {
-        op = context.operationName;
-      },
-      willSendResponse (context) {
-        const stop = Date.now();
-        const elapsed = stop - start;
-        const size = JSON.stringify(context.response).length * 2;
-        context.response.extensions.performance = {'queryRuntime': elapsed};
-        console.log(
-          `Operation ${op} completed in ${elapsed} ms and returned ${size} bytes`
-        );
-      }
-    };
-  },
-};
 
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers,
   plugins: [
-    LogPlugin,
+    plugins,
+    ApolloServerPluginInlineTrace(),
   ],
   formatResponse: (response, requestContext) => {
     //return response
     
     response = Object.assign(response, {
       extensions: {
-        meta: {
-          userId: 'aaa'
-        }
+        performance: {}
       }
     });
     return response;
