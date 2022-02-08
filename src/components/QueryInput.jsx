fe7c80b5551828ae39223caf71f6cefb14458b34
Irine Kang <irinekang@Irines-MacBook-Pro.local>
2022-02-08 10:26:25 -0600

    hardcoded graph display

--- src/components/QueryInput.jsx
+++ src/components/QueryInput.jsx
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 
 import CodeMirror from '@uiw/react-codemirror';
+import { ValidationContext, SDLValidationContext } from 'graphql';
 import { oneDark } from '@codemirror/theme-one-dark';
+import 'codemirror/keymap/sublime';
 import 'codemirror/addon/hint/show-hint';
 import 'codemirror/addon/lint/lint';
+import 'codemirror-graphql/hint';
 import 'codemirror-graphql/lint';
 import 'codemirror-graphql/mode';
+require('codemirror/mode/markdown/markdown');
 
 function QueryInput({ setQuery, setMutation }) {
   const [input, setInput] = useState('');
@@ -42,6 +46,8 @@ function QueryInput({ setQuery, setMutation }) {
     }
   };
 
+
+
   return (
     <div>
       <p>Query Input</p>
@@ -52,7 +58,15 @@ function QueryInput({ setQuery, setMutation }) {
         width='500px'
         onChange={(value) => handleChange(value)}
         theme={oneDark}
-        options={{ lineNumbers: true, mode: 'graphql', smartIndent: true, lineWrapping: true }}
+        options={{ 
+          theme: 'monokai',
+          lineNumbers: true, 
+          mode: 'graphql', 
+          lint: {
+            validationRules: []
+          },
+          smartIndent: true, 
+          lineWrapping: true }}
       />
       <Button variant="contained" className='submit-btn' onClick={handleSubmit}>Submit</Button>
       <Button variant="outline" onClick={reset}>Reset</Button>
