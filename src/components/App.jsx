ceaf02b2b5edd282928f40867d37b2b8b2ae942b
Irine Kang <irinekang@Irines-MacBook-Pro.local>
2022-02-09 10:59:10 -0600

    graph specified to 'Species'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,15 +40,22 @@ const CREATE_PERSON = gql`
 
 function App() {
   const [query, setQuery] = useState('');
+  const [mutation, setMutation] = useState('');
+  const [metrics, setMetrics] = useState({});
 
   return (
     <div>
+      <NavBar />
       <div className="main">
         <Schema />
         <QueryInput 
           setQuery={setQuery}
         />
-        <GraphData />
+        {
+          query ? <GraphData query={gql`${query}`} metrics={metrics}/> 
+            : mutation ? <GraphData mutation={gql`${mutation}`} /> 
+              : <p>No results to display...</p> 
+        }
         <p>GraphQL performance details</p>
         {
           // query ? <Detail query={gql`${query}`} /> 
--- src/components/GraphData.jsx
+++ src/components/GraphData.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { gql, useQuery } from '@apollo/client';
 import {Bar} from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 
-const state = {
-  labels: ['Resolver1', 'Resolver2', 'Resolver3', 'Resolver4', 'Resolver5', 'Resolver6'], //need to make this dynamically render w state
-  datasets: [
-    {
-      label: 'Speed',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [5, 3, 20, 5, 3, 20] //need to make this dynamically render w state
-    }
-  ]
-};
+function GraphData({Chart ,query, metrics}) {
+  let chartData;
+  const graphTag = [];
+  let keys, speed;
+  // console.log('data', data);
+  if (Object.keys(metrics).length > 0) {
+    console.log('metrics',  metrics);
 
-function GraphData({Chart}) {
-  return (
-    <div>
+
+    keys = Object.keys(metrics.Query.Species).filter((el)=>{
+      if (metrics.Query.Species[el].time !== undefined) return true;
+      else return false;
+    });
+    
+    console.log('keys', keys);
+  
+    speed = keys.map((el) => metrics.Query.Species[el].time[0]);
+    console.log('speed', speed);
+  
+
+    chartData =  {
+      labels: keys,
+      datasets: [{
+        label: 'Speed in ms',
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: speed
+      }]
+    };
+
+    console.log('first chartData', chartData);
+
+    graphTag.push(
       <Bar
-        data={state}
+        data={chartData}
         options={{
           title:{
             display:true,
@@ -33,6 +52,15 @@ function GraphData({Chart}) {
           indexAxis: 'y'
         }}
       />
+    );
+  } 
+
+  console.log('second chartData', chartData);
+
+
+  return (
+    <div>
+      {graphTag}
     </div>
   );
 }
