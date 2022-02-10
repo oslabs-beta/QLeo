9b5d617c4e49fc96003cab590fe21d7ada6a1bd4
Andrew Talle <talle.andrew@gmail.com>
2022-02-10 11:34:17 -0700

    refactor main layout with tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
     <div>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-32">
+          <div className="flex items-center justify-between h-20">
             <div className="flex items-center">
               <div className="flex-shrink-0">
                 <img
@@ -148,20 +148,6 @@ function NavBar() {
 
 
       </nav>
-
-      <header className="bg-white shadow">
-        <div className="max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-1xl font-bold text-gray-900">Dashboard</h1>
-        </div>
-      </header>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* <!-- Replace with your content --> */}
-          {/* <div className="px-4 py-6 sm:px-0"          iv className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div> */}
-          {/* <!-- /End replace --> */}
-        </div>
-      </main>
     </div>
   );
 }
