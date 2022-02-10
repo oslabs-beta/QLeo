a4016cdc99db2f9463fec88d2a68859e0324181c
Jack <jackcfitz1@gmail.com>
2022-02-10 13:24:14 -0700

    e

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
     <div>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-32">
+          <div className="flex items-center justify-between h-20">
             <div className="flex items-center">
               <div className="flex-shrink-0">
                 <img
