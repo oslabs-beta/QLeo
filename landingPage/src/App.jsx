2a9a0b21c37c6c37d34c37adb62e5c23ff51fae5
chon-h <83739431+chon-h@users.noreply.github.com>
2022-02-14 23:50:04 -0800

    added landing app (#44)

---
+++ landingPage/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Navbar from './Navbar.jsx';
+import FrontPage from './FrontPage.jsx';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <FrontPage />
+        </div>
+
+    )
+}
+
+export default App;
