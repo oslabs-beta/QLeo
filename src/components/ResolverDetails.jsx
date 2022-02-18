7e96117cd38088567b47678619ac1cd24ac272c3
Chon Hou Ho <chonhouh@gmail.com>
2022-02-18 14:28:38 -0800

    refactored dashboard ui

---
+++ src/components/ResolverDetails.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const PerofrmanceDetail = ({time, calls}) => {
+  return (
+    <div>
+      <p>Time: {time[0]}</p>
+      <p>Calls: {calls}</p>
+    </div>
+ );
+};
+
+const ResolverDetails = ({ obj, resolver }) => {
+  const [toggle, setToggle] = useState(false);
+  
+  return (
+    <div>
+      <p>{resolver} <button onClick={() => setToggle(!toggle)}>{toggle ? 'Hide details' : 'See details'}</button></p>
+      
+      {toggle && <PerofrmanceDetail time={obj.time} calls={obj.trips}/>}
+    </div>
+  );
+};
+
+export default ResolverDetails;
