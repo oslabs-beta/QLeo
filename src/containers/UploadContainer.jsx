4f7a8d37cd629eb383846c19895b25b68c4864c5
Andrew Talle <talle.andrew@gmail.com>
2022-02-18 18:05:00 -0700

    style upload page

--- src/containers/UploadContainer.jsx
+++ src/containers/UploadContainer.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import FileUpload from '../components/FileUpload';
+import lion from '../assets/Lion100px.svg';
+
 
 function UploadContainer() {
   return (
-    <div className="upload-container">
+    <div className="upload-container py-20 flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-between">
+        <img className="my-5 h-full w-full" src={lion} alt="logo" />
+        <h2 className='text-4xl'>Let&apos;s get started!</h2>
+      </div>
       <FileUpload />
     </div>
   );
