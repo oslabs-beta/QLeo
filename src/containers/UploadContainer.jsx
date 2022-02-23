1c1a0f43dbfd39e544d89231fa5c71a281812ae4
Andrew Talle <talle.andrew@gmail.com>
2022-02-23 11:34:27 -0700

    lifted schema state to app level in order to fix schema ui bug (#62)

--- src/containers/UploadContainer.jsx
+++ src/containers/UploadContainer.jsx
@@ -3,14 +3,14 @@ import FileUpload from '../components/FileUpload';
 import lion from '../assets/Lion100px.svg';
 
 
-function UploadContainer() {
+function UploadContainer({ schema, setSchema }) {
   return (
     <div className="upload-container py-20 flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-between">
         <img className="my-5 h-full w-full" src={lion} alt="logo" />
         <h2 className='text-3xl'>Let&apos;s get started!</h2>
       </div>
-      <FileUpload />
+      <FileUpload schema={schema} setSchema={setSchema} />
     </div>
   );
 }
